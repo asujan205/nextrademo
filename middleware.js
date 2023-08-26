cb2b484b0c1a8ad9bcbe52ef065c869c6f11df8d
asujan205 <asujan205@gmail.com>
2023-08-26 17:17:34 +0545

    middleware

--- middleware.js
+++ middleware.js
@@ -1,14 +1,14 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export default function middleware(req) {
-  let verify = req.cookies.get("authorization");
-  let url = req.url;
+// export default function middleware(req) {
+//   let verify = req.cookies.get("authorization");
+//   let url = req.url;
 
-  if (!verify && url.includes("/frameworks")) {
-    return NextResponse.redirect("http://localhost:3000/login/login");
-  }
+//   if (!verify && url.includes("/frameworks")) {
+//     return NextResponse.redirect("http://localhost:3000/login/login");
+//   }
 
-  if (verify && url === "http://localhost:3000/") {
-    return NextResponse.redirect("http://localhost:3000/frameworks/react");
-  }
-}
+//   if (verify && url === "http://localhost:3000/") {
+//     return NextResponse.redirect("http://localhost:3000/frameworks/react");
+//   }
+// }
